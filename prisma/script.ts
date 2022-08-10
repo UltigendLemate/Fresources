3554d4147234b2e72097ccfe082000a1ccd13976
korigamik <korigamik@gmail.com>
2022-08-11 02:46:15 +0530

    switch to mongo db add database schema

---
+++ prisma/script.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+  await prisma.college.create({
+    data: {
+      name: 'NSUT',
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
