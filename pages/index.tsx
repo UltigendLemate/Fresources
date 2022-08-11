28dcf58ade142e467881454e24f8f8fc9a017899
dhruv <dhruvbakshi12343@gmail.com>
2022-08-11 23:27:44 +0530

    added admin page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,247 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import { useRef, useState } from 'react';
+
+interface UploadItem {
+  name: string;
+  college: string;
+  course: string;
+  file: File;
+}
+
+const COLLEGE_LIST: { college: string; courses: string[] }[] = [
+  {
+    college: 'DTU',
+    courses: ['AC102', 'AC103', 'AC104', 'EE101', 'EE102', 'EE103'],
+  },
+  {
+    college: 'NSUT',
+    courses: ['AC102', 'AC103', 'AC104', 'EE101', 'EE102', 'EE103'],
+  },
+  {
+    college: 'IGDTUW',
+    courses: ['AC102', 'AC103', 'AC104', 'EE101', 'EE102', 'EE103'],
+  },
+  {
+    college: 'IIIT',
+    courses: ['AC102', 'AC103', 'AC104', 'EE101', 'EE102', 'EE103'],
+  },
+];
 
 const Home: NextPage = () => {
+  //states
+  const [progressState, setProgressState] = useState(0);
+  const [files, setFiles] = useState<UploadItem[]>([]);
+
+  //refs
+  const input_ref = useRef<HTMLInputElement>(null);
+
+  const upload = async (e: any) => {
+    for (let file of files) {
+      let xhr = new XMLHttpRequest();
+      let formData = new FormData();
+
+      formData.append(
+        'demo',
+        file.file,
+        `${file.college}/${file.course}/${file.name}`
+      );
+
+      xhr.upload.addEventListener('progress', (event) => {
+        if (event.lengthComputable) {
+          const progress = Math.round((event.loaded * 100) / event.total);
+          setProgressState(progress);
+        }
+      });
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          setProgressState(0);
+
+          if (xhr.status >= 200 && xhr.status < 300) {
+            console.log('done');
+            setFiles((prev) => {
+              return prev.filter((f) => f !== file);
+            });
+          } else {
+            console.log('error');
+          }
+        }
+      };
+      xhr.open('POST', '/api/upload', true);
+      // xhr.withCredentials = props.withCredentials
+      xhr.send(formData);
+    }
+  };
+
+  const handleAddFile = (event: any) => {
+    let fileArray: UploadItem[] = [];
+    for (let file of event.target.files) {
+      fileArray.push({
+        file,
+        name: file.name,
+        college: 'DTU',
+        course: 'AC102',
+      });
+    }
+    setFiles((prev) => {
+      return [...prev, ...fileArray];
+    });
+  };
+
+  const calculateFileSize = (bytes: number) => {
+    if (bytes === 0) {
+      return '0 B';
+    }
+    let k = 1000,
+      dm = 3,
+      sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+      i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  };
+
+  const nameChangeHandler = (name: string, index: number) => {
+    setFiles((prev) => {
+      const extention = prev[index].name.split('.').pop();
+      console.log(`${name}.${extention}`);
+      return [
+        ...prev.slice(0, index),
+        { ...prev[index], name: `${name}.${extention}` },
+        ...prev.slice(index + 1),
+      ];
+    });
+  };
+
+  const collegeChangeHandler = (college: string, index: number) => {
+    setFiles((prev) => {
+      return [
+        ...prev.slice(0, index),
+        { ...prev[index], college },
+        ...prev.slice(index + 1),
+      ];
+    });
+  };
+
+  console.log(files);
+
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <div>
+      <input
+        type={'file'}
+        multiple
+        onChange={handleAddFile}
+        ref={input_ref}
+        className='hidden'
+      />
+      {/* <div className='text-2xl'>{progressState}</div> */}
 
-      <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
-        <h1 className='text-6xl font-bold'>
-          Welcome to{' '}
-          <a className='text-blue-600' href='https://nextjs.org'>
-            Next.js!
-          </a>
-        </h1>
-
-        <p className='mt-3 text-2xl'>
-          Get started by editing{' '}
-          <code className='rounded-md bg-gray-100 p-3 font-mono text-lg'>
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className='mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full'>
-          <a
-            href='https://nextjs.org/docs'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
+      <div className='px-4 py-4'>
+        <div id='options' className='flex gap-4'>
+          <div
+            onClick={() => input_ref.current?.click()}
+            className='bg-blue-500 text-white text-2xl font-bold w-fit py-2 px-6 rounded-lg cursor-pointer'
           >
-            <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href='https://nextjs.org/learn'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
+            Choose
+          </div>
+          <button
+            onClick={upload}
+            className={`${
+              files.length > 0
+                ? 'bg-blue-500'
+                : 'bg-blue-300 cursor-not-allowed'
+            } text-white text-2xl font-bold w-fit py-2 px-6 rounded-lg cursor-pointer`}
           >
-            <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/canary/examples'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
+            Upload
+          </button>
+          <div
+            onClick={() => setFiles([])}
+            className={`${
+              files.length > 0
+                ? 'bg-blue-500'
+                : 'bg-blue-300 cursor-not-allowed'
+            } text-white text-2xl font-bold w-fit py-2 px-6 rounded-lg cursor-pointer`}
           >
-            <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Cancel
+          </div>
         </div>
-      </main>
-
-      <footer className='flex h-24 w-full items-center justify-center border-t'>
-        <a
-          className='flex items-center justify-center gap-2'
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </a>
-      </footer>
+        <div className=' flex flex-col gap-2 mt-4 w-full'>
+          {files
+            .map((file, idx) => {
+              return (
+                <div key={idx}>
+                  <div className='py-4 px-4 rounded-lg bg-slate-900 w-full text-white flex justify-between'>
+                    <div>
+                      {file.name} ({calculateFileSize(file.file.size)})
+                    </div>
+                    <div className='flex gap-2'>
+                      <select
+                        className='text-black'
+                        onClick={(e: any) => {
+                          collegeChangeHandler(e.target?.value, idx);
+                        }}
+                      >
+                        {COLLEGE_LIST.map((college) => {
+                          return (
+                            <option
+                              key={college.college}
+                              value={college.college}
+                            >
+                              {college.college}
+                            </option>
+                          );
+                        })}
+                      </select>
+                      {/* <select
+                        className='text-black'
+                        onClick={(e: any) => {
+                          collegeChangeHandler(e.target?.value, idx)
+                        }}
+                      >
+                        {COLLEGE_LIST.map((college) => {
+                          return (
+                            <option
+                              key={college.college}
+                              value={college.college}
+                            >
+                              {college.college}
+                            </option>
+                          )
+                        })}
+                      </select> */}
+                      <div className='bg-blue-700 px-3 rounded-lg cursor-pointer'>
+                        Edit Name
+                      </div>
+                      <div
+                        className='bg-red-700 px-3 rounded-lg cursor-pointer'
+                        onClick={() => {
+                          setFiles((prev) => {
+                            return [
+                              ...prev.slice(0, idx),
+                              ...prev.slice(idx + 1),
+                            ];
+                          });
+                        }}
+                      >
+                        Remove
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex'>
+                    <input
+                      type={'text'}
+                      className='border-2 rounded-l-lg px-4'
+                      onChange={(e) => nameChangeHandler(e.target.value, idx)}
+                    />
+                    <button className='border-2 px-4 py-2 rounded-r-lg border-l-0 bg-blue-800 text-white'>
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              );
+            })
+            .reverse()}
+        </div>
+      </div>
     </div>
   );
 };
