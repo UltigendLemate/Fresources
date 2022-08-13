bf744199b6f8a792ddd7930a6cc782a9e254c3ea
dhruv <dhruvbakshi12343@gmail.com>
2022-08-13 12:47:00 +0530

    removed package.json from gitignore

--- pages/[college]/index.tsx
+++ pages/[college]/index.tsx
@@ -2,10 +2,10 @@ import { GetStaticProps } from 'next/types'
 import { ParsedUrlQuery } from 'querystring'
 import { prisma } from '../../src/prisma'
 
-type Props = {college: string }
+type Props = { college: string }
 
 interface IParams extends ParsedUrlQuery {
-	college: string
+  college: string
 }
 
 const College = (props: Props) => {
@@ -14,14 +14,14 @@ const College = (props: Props) => {
 
 export const getStaticPaths = async () => {
   const colleges = await prisma.college.findMany()
-	console.log(colleges)
+  console.log(colleges)
   const paths = colleges.map((college) => {
     return {
-      params: {college: college.name },
+      params: { college: college.name },
     }
   })
 
-  return { paths, fallback: false}
+  return { paths, fallback: false }
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
