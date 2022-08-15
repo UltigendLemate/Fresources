cfbc4afdd2bd164662290015f37c7d6bc201f5c0
GargDivanshu <divanshu.garg113@gmail.com>
2022-08-16 02:51:56 +0530

    dropdown

--- components/utility/Button/Glass.tsx
+++ components/utility/Button/Glass.tsx
@@ -11,7 +11,8 @@ const Glass: React.FC<GlassPropsType> = (
   props: GlassPropsType
 ): JSX.Element => {
   return (
-    <Link href={props.href as string}>
+    <Link href={props.href ? props.href:""} >
+     
       <button
         className={`w-full px-8 py-4 md:py-4 md:px-6 rounded-xl font-bold text-2xl glass shadow-[rgba(255,255,255,0.50)] duration-300 transition border-2 border-transparent hover:border-[#f5a607] ${
           props.css && props.css
