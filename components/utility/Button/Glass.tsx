2f0b5517e54c200cf4a47b439aa0c3d451b99f69
Your Name <you@example.com>
2022-08-31 19:25:42 +0530

    tooltip fixed

--- components/utility/Button/Glass.tsx
+++ components/utility/Button/Glass.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactTooltip from 'react-tooltip'
 
 export type GlassPropsType = {
@@ -26,28 +26,42 @@ const Glass: React.FC<GlassPropsType> = ({
   tooltip = true,
   titlecase = true,
 }: GlassPropsType): JSX.Element => {
+
+
+
   const [isHover, setIsHover] = useState(false)
+  const [toolShow, setToolShow] = useState(true)
+  const tool = () => {
+    if (typeof window !== 'undefined') {
+      if (window.innerWidth > 640) {
+        setToolShow(true)
+      } else {
+        setToolShow(false)
+      }
+    }
+  }
+  useEffect(() => {
+    tool()
+  }, [])
 
   return (
     <div>
       <button
         data-tip
         data-for={value}
-        className={`w-full px-8 py-4 md:px-6 rounded-xl sm:font-bold text-2xl glass shadow-[rgba(255,255,255,0.50)] duration-300 transition border-2 border-transparent hover:border-[#f5a607] ${
-          css && css
-        }`}
-        onMouseEnter={() => setIsHover(true)}
+        className={`w-full px-8 py-4 md:px-6 rounded-xl sm:font-bold text-2xl glass shadow-[rgba(255,255,255,0.50)] duration-300 transition border-2 border-transparent hover:border-[#f5a607] ${css && css
+          }`}
+        onMouseEnter={() => setIsHover(toolShow)}
         onMouseLeave={() => setIsHover(false)}
       >
         {titlecase ? Abbreviate(value, titlecase) : value}
       </button>
       <div
-        className={`${
-          (!isHover ||
-            !tooltip ||
-            toTitleCase(value, titlecase) === Abbreviate(value, titlecase)) &&
+        className={`${(!isHover ||
+          !tooltip ||
+          toTitleCase(value, titlecase) === Abbreviate(value, titlecase)) &&
           'hidden'
-        } bg-[#000000a3]`}
+          } bg-[#000000a3]`}
       >
         <ReactTooltip
           id={value}
@@ -58,7 +72,7 @@ const Glass: React.FC<GlassPropsType> = ({
           <span className='text-xl'>{toTitleCase(value, titlecase)}</span>
         </ReactTooltip>
       </div>
-    </div>
+    </div >
   )
 }
 
