67fce445dd64ab3522e3f33fa59c5fd4e0334481
Your Name <you@example.com>
2022-08-31 21:51:52 +0530

    pdf error

--- components/utility/PdfViewer/index.tsx
+++ components/utility/PdfViewer/index.tsx
@@ -2,7 +2,7 @@ import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import {
   defaultLayoutPlugin,
-  ToolbarSlot
+  ToolbarSlot,
 } from '@react-pdf-viewer/default-layout'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
